460e414e0fe98c48a91c5961243f5705211484f3
Pratik <pratikjariwala6696@gmail.com>
2022-03-04 20:57:11 +0530

    dotenv added

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const dbURI = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const dbURI = config.get("mongoURI");
+const dotenv = require("dotenv");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(dbURI, {
-            useNewUrlParser: true
-        });
-        console.log("CONNECTED");
-    } catch (err) {
-        console.error(err.message);
-    }
-}
+  try {
+    await mongoose.connect(process.env.mongoURI, {
+      useNewUrlParser: true,
+    });
+    console.log("CONNECTED");
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
 module.exports = connectDB;
